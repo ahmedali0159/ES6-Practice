0b9742ee4f772b7f848c6c94591397eff2f1047d
Ahmed Ali <pirates0159@gmail.com>
2021-02-02 14:32:50 +0600

    practice

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = "Abu";
+const lastName = "Bakar";
+const fullName = firstName + " " + lastName + " is a good person of the world";
+const fullName2 = `${firstName} ${lastName} is a good person of the world`;
+
+
+const multiLine = " i love him\n"
+     + "I miss him\n"
+     + "I need him";
+
+const multiLine2 = `I love You
+I miss you
+I need you.`
+console.log(multiLine2);
